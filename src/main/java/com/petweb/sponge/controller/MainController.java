e7664733243ce0dc85078ec1d6cdeac65068f99a
arsgsg1 <arsgsg1@gmail.com>
2024-07-14 13:26:13 +0900

    ci-cd 테스트

--- src/main/java/com/petweb/sponge/controller/MainController.java
+++ src/main/java/com/petweb/sponge/controller/MainController.java
@@ -1,4 +1,4 @@
-package com.petweb.sponge;
+package com.petweb.sponge.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
